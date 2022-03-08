7e31a87dbe1b102dbd322028d00036c3ba22ca29
Alan Seigler <alan.seigler@hotmail.fr>
2022-03-08 14:44:25 +0100

    setup blog backoffice

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
---
+++ src/Admin/ArticleAdmin.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+final class ArticleAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->with('Content', ['class' => 'col-md-9'])
+            ->add('title', TextType::class)
+            ->add('body', TextareaType::class)
+            ->end()
+            ->with('Meta Data', ['class' => 'col-md-3'])
+            ->add('category', ModelType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+            ->end();
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagrid): void
+    {
+        $datagrid->add('title');
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list->addIdentifier('title');
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show->add('title');
+    }
+
+    public function toString(object $object): string
+    {
+        return $object instanceof Article
+            ? $object->getTitle()
+            : 'Article'; // shown in the breadcrumb on the create view
+    }
+}
