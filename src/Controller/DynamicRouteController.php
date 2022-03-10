68e8d70477dd3dc6d78ef102328b7d5a387f10eb
Tom Cuillandre <tcuillandre@myges.fr>
2022-03-10 16:01:52 +0100

    Add dynamic route from page slug

---
+++ src/Controller/DynamicRouteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DynamicRouteController extends AbstractController
+{
+    #[Route('/{slug}', name: 'dynamic_route', priority:-1)]
+    public function index($slug, PageRepository $pageRepository): Response
+    {
+        $page = $pageRepository->findOneBy(['slug' => $slug]);
+
+        if($page){
+            return $this->render('front/dynamic.route.html.twig', ['slug' => $slug, 'blocks' => $page->getBlockList()]);
+        }
+
+        return $this->redirectToRoute('home');
+    }
+}
